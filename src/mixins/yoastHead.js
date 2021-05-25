fc335cbb67b690a87c00421791999af733dae86f
2Cubed-ie <admin@2cubed.ie>
2021-05-25 10:34:10 +0100

    Fix yoast head not adding meta tags

--- src/mixins/yoastHead.js
+++ src/mixins/yoastHead.js
@@ -13,7 +13,7 @@ export const yoastHead = {
   metaInfo() {
     return {
       title: this.scTitle,
-      meta: this.scScript,
+      meta: this.scMeta,
       script: this.scScript,
     };
   },
@@ -33,10 +33,16 @@ export const yoastHead = {
         allowedAttributes: false,
       });
       const rawJson = this.converter.html2json(cleanHtml);
-      this.scTitle = rawJson.child.map((el) => (el.tag === "title" ? el.child[0].text : null)).filter((el) => el)[0];
+      this.scTitle = rawJson.child
+        .map((el) => (el.tag === "title" ? el.child[0].text : null))
+        .filter((el) => el)[0];
       const elements = rawJson.child.filter((tag) => tag.node === "element");
-      const metasArray = elements.filter((m) => m.tag === "meta").map((m) => m.attr);
-      const scriptsArray = elements.filter((s) => s.tag === "script").map((s) => {
+      const metasArray = elements
+        .filter((m) => m.tag === "meta")
+        .map((m) => m.attr);
+      const scriptsArray = elements
+        .filter((s) => s.tag === "script")
+        .map((s) => {
           return {
             innerHTML: s.attr.type,
             type: s.child[0].text,
@@ -46,9 +52,16 @@ export const yoastHead = {
       let meta = [];
       for (let i = 0; i < metasArray.length; i++) {
         let obj = { name: "", content: "", key: "" };
-        obj.name = metasArray[i].property ? metasArray[i].property : metasArray[i].name;
-        obj.content = typeof metasArray[i].content === "string" ? metasArray[i].content : metasArray[i].content.join(" ");
-        obj.key = metasArray[i].property ? metasArray[i].property : metasArray[i].name;
+        obj.name = metasArray[i].property
+          ? metasArray[i].property
+          : metasArray[i].name;
+        obj.content =
+          typeof metasArray[i].content === "string"
+            ? metasArray[i].content
+            : metasArray[i].content.join(" ");
+        obj.key = metasArray[i].property
+          ? metasArray[i].property
+          : metasArray[i].name;
         meta.push(obj);
       }
       this.scScript = scriptsArray;
