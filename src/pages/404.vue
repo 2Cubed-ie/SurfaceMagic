72653333c41d39368180094f18bf45f24cade723
Scott Kennedy <scottyzen@gmail.com>
2021-06-01 21:12:38 +0100

    add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <Layout>
+    <div
+      class="container p-4 my-24 font-semibold text-center text-primary-dark"
+    >
+      <h1 class="text-5xl">Error 404</h1>
+      <h2 class="mb-12 text-3xl">
+        Page can not be found.
+      </h2>
+      <g-link to="/" class="button">Return Home</g-link>
+    </div>
+  </Layout>
+</template>
