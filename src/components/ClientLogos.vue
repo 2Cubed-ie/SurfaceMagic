f09c80c0a7c8885079be1a9041d6b6d545f2a57e
2Cubed-ie <admin@2cubed.ie>
2021-06-14 15:21:05 +0100

    Fixed client logos to better fit

--- src/components/ClientLogos.vue
+++ src/components/ClientLogos.vue
@@ -1,17 +1,13 @@
 <template>
   <div
-    class="flex flex-wrap items-center gap-4  justify-evenly sm:flex-nowrap md:justify-between"
+    class="flex flex-wrap items-center gap-4 justify-evenly md:justify-between"
   >
     <div
       class="max-w-sm mb-4"
       v-for="({ logo }, index) in $static.wordPressPage.acf.ourClients"
       :key="index"
     >
-      <GImage
-        :src="logo.downloaded"
-        :alt="logo.altText"
-        :title="logo.title"
-      />
+      <GImage :src="logo.downloaded" :alt="logo.altText" :title="logo.title" />
     </div>
   </div>
 </template>
@@ -23,7 +19,7 @@ query {
     acf{
       ourClients{
         logo{
-          downloaded(width: 140, height: 140, quality: 95)
+          downloaded(width: 120, height: 120, quality: 95, fit: inside)
           title
           altText
         }
